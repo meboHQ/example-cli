a4135b903c28526e891a7c818b7c21f21b34686b
Paulo Nogueira <paulondc@gmail.com>
2018-08-09 15:54:58 -0700

    updated documentation

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ require('./ResizeImage');
 // node . --cli resizeImage --width=50 --height=50 data/logo.png > /tmp/logoTarget.png
 if (process.argv.includes('--cli')) {
 
+  // for this example we could be executing resizeImage directly, since this is the
+  // only action. However, keeping as demonstration the logic about how to query the
+  // action name from the args (in case you want to provide more actions through
+  // cli handler)
   const actionName = process.argv[process.argv.indexOf('--cli')];
 
   // creating a command-line handler which is used to load the command-line
