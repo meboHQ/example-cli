f3cf03faf2544091373f8c9538970d7095817fd7
Paulo Nogueira <paulondc@gmail.com>
2018-08-13 19:29:39 -0700

    Update Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,7 @@ const Mebo = require('mebo');
 // registering action
 @Mebo.grant('cli')
 @Mebo.register('hello')
-class ResizeImage extends Mebo.Action{
+class Hello extends Mebo.Action{
   constructor(){
     super();
     this.createInput('content?: text', {defaultValue: 'Hello, World!', elementType: "argument", description: "output text"});
@@ -15,4 +15,4 @@ class ResizeImage extends Mebo.Action{
   }
 }
 
-module.exports = ResizeImage;
+module.exports = Hello;
