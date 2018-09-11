979b04eb143fe0517ba4afa50c99479928e0b3cd
Paulo Nogueira <paulondc@gmail.com>
2018-09-11 16:30:22 -0700

    Update Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -7,7 +7,7 @@ class Hello extends Mebo.Action{
   constructor(){
     super();
     this.createInput('content?: text', {defaultValue: 'Hello, World!', elementType: "argument", description: "output text"});
-    this.setMeta('$cliDescription', "Example printing hello world");
+    this.setMeta('description', "Example printing hello world");
   }
 
   async _perform(data){
